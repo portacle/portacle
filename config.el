08086ab688acb24ce72e2ebe492bc019fbd33b6e
Shinmera <shinmera@tymoon.eu>
2016-05-25 10:34:27 +0200

    Default config.el doesn't do anything

--- config.el
+++ config.el
@@ -1,9 +1,9 @@
-;; This is the list of directories that the source browser displays
-;; on the left.
-(setq ecb-source-path `("~/"
-                        ,portacl-root
-                        ,(concat portacl-root "/sbcl/sources/")))
+;;;; This is the variable that contains the default directories listed in the browser.
+;;;; You can uncomment and change this to substitute your own.
+;; (setq ecb-source-path `("~/"
+;;                         ,portacl-root
+;;                         ,(concat portacl-root "/sbcl/sources/")))
 
-;; This is the default font that is used globally. Adjust it to
-;; one that you like. Consolas is nice, for example.
-(set-frame-font "Monospace-10" nil t)
+;;;; This is the default font that is used globally. Adjust it to
+;;;; one that you like. Consolas is nice, for example.
+;; (set-frame-font "Monospace-10" nil t)
