c881cccdb34bc95184532baa528437562bc987bb
Shinmera <shinmera@tymoon.eu>
2016-05-25 11:31:02 +0200

    Run emacs as part of the emacsd step.

--- build/emacsd.sh
+++ build/emacsd.sh
@@ -12,6 +12,15 @@ function install (){
     mkdir -p "$INSTALL_TARGET"
     cp -R -t "$INSTALL_TARGET" "$SOURCE_DIR/."
     ## We don't remove the .git directory so that you can update easily.
+    echo ""
+    echo ";;;; Now launching emacs for the first time init."
+    echo ";; It will download the necessary libraries and compile them."
+    echo ";; Please be patient, it should automatically close when it is done."
+
+    case "$PLATFORM" in
+        lin) $SCRIPT_DIR/../emacs/$PLATFORM/emacs.sh -nw -f "kill-emacs" ;;
+        win) cmd $SCRIPT_DIR/../emacs/$PLATFORM/emacs.bat -nw -f "kill-emacs" ;;
+    esac
 }
 
 main
