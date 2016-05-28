dd673de80d9bc72e38b1b154e621e3dbb12fb05b
Shinmera <shinmera@tymoon.eu>
2016-05-29 00:29:05 +0200

    Added magit, fixed some other guff.

--- config/emacs-init.el
+++ config/emacs-init.el
@@ -2,26 +2,29 @@
 
 ;; Set up paths
 (setq portacl-root (or (getenv "ROOT") (expand-file-name "~/")))
-(setq user-emacs-directory (concat portacl-root "emacs/config/"))
+
+(defun portacl-path (path)
+  (concat portacl-root path))
+
+(setq user-emacs-directory (portacl-path "emacs/config/"))
 (add-to-list 'load-path (concat user-emacs-directory "shinmera/"))
 
 ;; Load contribs
 (require 'shinmera-general)
+(require 'shinmera-functions)
 (require 'shinmera-neotree)
 (require 'shinmera-company)
 (require 'shinmera-paste)
 (require 'shinmera-keys)
+(require 'shinmera-magit)
 (require 'shinmera-lisp)
 (require 'shinmera-startup)
 
 ;; Make sure SLIME knows about our SBCL
 (setq slime-lisp-implementations
-      `((sbcl (,(concat portacl-root (cond ((eql system-type 'gnu/linux)
-                                            "/sbcl/lin/sbcl.sh")
-                                           ((eql system-type 'darwin)
-                                            "/sbcl/mac/sbcl.sh")
-                                           ((eql system-type 'windows-nt)
-                                            "/sbcl/win/sbcl.bat")))))))
+      `((sbcl (,(portacl-path (cond ((eql system-type 'gnu/linux)  "sbcl/lin/sbcl.sh")
+                                    ((eql system-type 'darwin)     "sbcl/mac/sbcl.sh")
+                                    ((eql system-type 'windows-nt) "sbcl/win/sbcl.bat")))))))
 
 (when window-system
   (toggle-frame-maximized)
@@ -35,7 +38,7 @@
 
 ;; Open the help file
 (with-current-buffer (get-buffer-create "*portacle-help*")
-  (insert-file-contents (concat portacl-root "config/help.txt"))
+  (insert-file-contents (portacl-path "config/help.txt"))
   (beginning-of-buffer))
 
 ;; Customise the scratch buffer
@@ -51,8 +54,22 @@
 ")
 (setq initial-major-mode 'common-lisp-mode)
 
+;; Make sure we have GIT in the path
+(add-to-path (portacl-path (cond ((eql system-type 'gnu/linux)  "git/lin/bin")
+                                 ((eql system-type 'darwin)     "git/mac/bin")
+                                 ((eql system-type 'windows-nt) "git/win/bin")))
+             (portacl-path (cond ((eql system-type 'gnu/linux)  "git/lin/libexec/git-core")
+                                 ((eql system-type 'darwin)     "git/mac/libexec/git-core")
+                                 ((eql system-type 'windows-nt) "git/win/libexec/git-core"))))
+
+;; But just to make doubly sure we'll tell Magit explicitly
+(setq magit-git-executable
+      (portacl-path (cond ((eql system-type 'gnu/linux)  "git/lin/bin/git")
+                          ((eql system-type 'darwin)     "git/mac/bin/git")
+                          ((eql system-type 'windows-nt) "git/win/bin/git.exe"))))
+
 ;; Load user file
-(load (concat portacl-root "config/user.el"))
+(load (portacl-path "config/user.el"))
 
 ;; Trigger contrib startup
 (startup-shinmera)
