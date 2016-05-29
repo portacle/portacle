c7c4c76adb710e4dc70eefc534fcd2304b206980
Shinmera <shinmera@tymoon.eu>
2016-05-29 11:33:14 +0200

    Attempted fix for git launcher

--- git/win/git.bat
+++ git/win/git.bat
@@ -5,6 +5,6 @@ set ROOT=%SCRIPT%\..\..\
 rem // This crazy thing resolves the relative path
 FOR /F "delims=" %%F IN ("%ROOT%") DO SET "ROOT=%%~fF"
     
-set PATH=%PATH%;%ROOT%\git\win\libexec\git-core
+set PATH=%PATH%;%ROOT%git\win\bin;%ROOT%git\win\libexec\git-core
 
-"%SCRIPT%\bin\git" %*
+"%ROOT%git\win\bin\git.exe" %*
