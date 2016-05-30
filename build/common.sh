4e2f2e71564440d98a77faef031148ddf7bc1ac2
Shinmera <shinmera@tymoon.eu>
2016-05-30 11:10:32 +0200

    Reverse order, fix thing to actually match

--- build/common.sh
+++ build/common.sh
@@ -37,7 +37,7 @@ esac
 ## Find all the dynamic libraries not provided by the native OS
 function nonlocal-ldd() {
     case "$PLATFORM" in
-        win) ldd "$1" | grep -E '^/[a-z]{2,}/$' | awk -F\  '{print $3}' ;;
+        win) ldd "$1" | awk '{print $3}' | grep -E '^/[a-z]{2,}/' ;;
         mac) otool -L "$1" | grep -E "/opt/local/lib|/usr/local/lib" | awk -F\  '{print $1}' ;;
         *) ;;
     esac
