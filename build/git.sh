ccce52c588bebb53786c85fabca5a8f27954920f
Shinmera <shinmera@tymoon.eu>
2016-06-02 20:38:47 +0200

    Converting hardlinks manually is not necessary anymore with NO_INSTALL_HARDLINKS

--- build/git.sh
+++ build/git.sh
@@ -24,18 +24,6 @@ function build() {
         || eexit "The build failed. Please check the output for error messages."
 }
 
-function win-convert-hardlinks() {
-    local links=( $(find "$INSTALL_TARGET/" -samefile "$INSTALL_TARGET/bin/git.exe") )
-    for link in "${links[@]}"; do
-        if [ "$link" != "$INSTALL_TARGET/bin/git.exe" ]; then
-            local winpath=$(cygpath -w "$link")
-            eecho "Converting hard link $link"
-            rm "$link"
-            cmd /c "mklink \"$winpath\" \"..\\..\\bin\\git.exe\""
-        fi
-    done
-}
-
 function win-copy-coreutils() {
     local coreutils=( $(pacman -Ql coreutils | grep "exe" | awk '{print $2}') )
     ensure-installed "$INSTALL_TARGET/bin/" "/usr/bin/bash.exe" "/usr/bin/sh.exe" "${coreutils[@]}"
@@ -47,8 +35,7 @@ function install() {
         || eexit "The install failed. Please check the output for error messages."
 
     case "$PLATFORM" in
-        win) win-convert-hardlinks
-             win-copy-coreutils
+        win) win-copy-coreutils
              ensure-installed "$SHARED_DIR" "/mingw64/bin/libcurl-4.dll"
              ensure-dependencies "/mingw64/bin/libcurl-4.dll"
              ;;
