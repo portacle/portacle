31e0228e2ef95785440c51b7bfef956f157372bf
Shinmera <shinmera@tymoon.eu>
2016-06-03 11:26:55 +0200

    Don't chdir after chroot.

--- git/win/bash.bat
+++ git/win/bash.bat
@@ -9,5 +9,5 @@ FOR /F "delims=" %%F IN ("%ROOT%") DO SET "ROOT=%%~fF"
 set BASHPATH=%BASHPATH%;%ROOT%\shared\lib;%ROOT%\shared\bin
 set PATH=%BASHPATH%
 
-"%ROOT%\shared\bin\chroot.exe" %ROOT% "/shared/bin/bash.exe" %*
+"%ROOT%\shared\bin\chroot.exe" "--skip-chdir" %ROOT% "/shared/bin/bash.exe" %*
 ENDLOCAL
