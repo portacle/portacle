c5c3f57f7b1486d4a70f736dfd8b186e2b9693ed
Shinmera <shinmera@tymoon.eu>
2016-06-03 22:30:07 +0200

    Create the temp dir to stop bash from complaining on windows

--- build/git.sh
+++ build/git.sh
@@ -50,6 +50,7 @@ function install() {
              ensure-installed "$SHARED_DIR/lib/" "/mingw64/bin/libcurl-4.dll"
              ensure-installed "$SHARED_DIR/share/" "/usr/lib/terminfo"
              ensure-dependencies "/mingw64/bin/libcurl-4.dll"
+             mkdir -p "$PORTACLE_DIR/tmp"
              ;;
         lin) ensure-installed "$SHARED_DIR/lib/" "/usr/lib/libcurl.so"
              ensure-dependencies "/usr/lib/libcurl.so"
