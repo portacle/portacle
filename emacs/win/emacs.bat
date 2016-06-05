54583b2995af0e8bf5b9c5dd6fe4dfcf2c01304f
Shinmera <shinmera@tymoon.eu>
2016-06-05 23:27:41 +0200

    Revert attempt at fixing arg passing because it doesn't help at all.

--- emacs/win/emacs.bat
+++ emacs/win/emacs.bat
@@ -1,7 +1,5 @@
 @echo off
 SETLOCAL
-set ARGS=%*
-set ARGS=%ARGS:$=`$%
 
 set SCRIPT=%~dp0
 set ROOT=%SCRIPT%\..\..\
@@ -46,5 +44,5 @@ set EMACSLOADPATH=%ROOT%\emacs\share\emacs\%EMACSVER%\site-lisp;^
 set PATH=%ROOT%\emacs\win\libexec\emacs\%EMACSVER%\x86_64-w64-mingw32;%PATH%
 set PATH=%ROOT%\usr\lib\;%PATH%
 
-"%SCRIPT%\bin\emacs" --name Portacle -T Portacle -q -l "%ROOT%\config\emacs-init.el" %ARGS
+"%SCRIPT%\bin\emacs" --name Portacle -T Portacle -q -l "%ROOT%\config\emacs-init.el" %*
 ENDLOCAL
