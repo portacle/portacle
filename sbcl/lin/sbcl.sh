b6ae0d60e5bf8a886e98b12a3eca3d96cfcef88d
Shinmera <shinmera@tymoon.eu>
2016-06-09 20:06:11 +0200

    Note about ld-linux.so being weird with sbcl, potential fix.

--- sbcl/lin/sbcl.sh
+++ sbcl/lin/sbcl.sh
@@ -3,4 +3,11 @@ SCRIPT=$(dirname $(readlink -f "$0"))
 export ROOT=${ROOT:-$(readlink -f "$SCRIPT/../../")/}
 export SBCL_HOME=$ROOT/sbcl/lin/lib/sbcl/
 
-"$ROOT/usr/lib/ld-linux.so" --library-path "$ROOT/usr/lib/" "$SCRIPT/bin/sbcl" --no-sysinit --userinit "$ROOT/config/sbcl-init.lisp" "$@"
+## The following does not work because ???
+## ld-linux.so just seems to sweep past the sbcl binary argument
+## for reasons that are completely beyond me. I cannot figure out
+## how to fix that behaviour either, so we revert to using system
+## provided libs and hoping things work out.
+# "$ROOT/usr/lib/ld-linux.so" --library-path "$ROOT/usr/lib/" "$SCRIPT/bin/sbcl" --no-sysinit --userinit "$ROOT/config/sbcl-init.lisp" "$@"
+
+"$SCRIPT/bin/sbcl" --no-sysinit --userinit "$ROOT/config/sbcl-init.lisp" "$@"
