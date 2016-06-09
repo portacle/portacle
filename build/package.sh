238ff2a37d4fc2d12a784fd8f6fe8e41e2555abe
Shinmera <shinmera@tymoon.eu>
2016-06-09 14:13:32 +0200

    Fixed packaging to work as we want it to on unixes.

--- build/package.sh
+++ build/package.sh
@@ -6,7 +6,7 @@ PACKAGE_DIR=$PORTACLE_DIR/package/
 PACKAGE_VERS=$(git rev-parse --short HEAD)
 PACKAGE_FILE=$PLATFORM-portacle-$PACKAGE_VERS
 
-function win-translate-names() {
+function win-translate-paths() {
     local result=()
     for file in "$@"; do
         result+=($(cygpath -w "$file"))
@@ -14,32 +14,60 @@ function win-translate-names() {
     echo "${result[@]}"
 }
 
-function create-package() {
-    local file="$1"
-    local dir=$(dirname "$file")
-    mkdir -p "$dir"
-    cd "$PORTACLE_DIR"
-    local files=($(discover-files "."))
+function relativise-paths() {
+    local result=()
+    for file in "$@"; do
+        result+=($(basename "$file"))
+    done
+    echo "${result[@]}"
+}
 
+function package-files(){
+    local package="$1"
+    local files=("${@:2}")
+    
     case "$PLATFORM" in
-        win) local winfile=$(win-translate-names "$file")
-             local winfiles=($(win-translate-names "${files[@]}"))
-             eexit "THIS DOES NOT DO WHAT YOU WANT YET BECAUSE I CAN'T FUCKING GET THE 7ZIP COMMAND LINE OPTIONS TO WORK."
+        win) local winfile=$(win-translate-paths "$package")
+             local winfiles=($(win-translate-paths "$files"))
              "/c/Program Files/7-zip/7z.exe" a -t7z "$winfile.exe" -m0=LZMA2 -mmt2 -sfx7z.sfx -aoa -r -snh -snl -ssw -y "${winfiles[@]}" \
-                   || eexit "Could not create package."
+                 || eexit "Could not create package."
              ;;
-        *)   tar -cJf "$file.tar.xz" "${files[@]}" \
+        *)   tar -cJf "$package.tar.xz" "${files[@]}" \
                  || eexit "Could not create package."
              ;;
     esac
+}
+
+function package-portacle-dir() {
+    local package="$1"
+    local dir="$2"
+    mkdir -p $(dirname "$package")
+    cd $(dirname "$dir")
+
+    package-files "$package" $(basename "$dir")
 }
 
 function discover-files() {
     ls -rt -d -1 "$1/"{*,.*} | egrep -v "build|package|/\\.{1,2}$"
 }
 
-function package-up() {
-    create-package "$PACKAGE_DIR/$PACKAGE_FILE"
+function prepare-for-packaging() {
+    local source="$1"
+    local target="$2"
+    local files=($(discover-files "$source"))
+
+    mkdir -p "$target"
+    if system-has rsync; then
+        rsync -avz "${files[@]}" "$target"
+    else
+        cp -rfuv "${files[@]}" "$target"
+    fi
 }
 
-package-up
+function package() {
+    local target="$PACKAGE_DIR/portacle"
+    prepare-for-packaging "$PORTACLE_DIR" "$target"
+    package-portacle-dir "$PACKAGE_DIR/$PACKAGE_FILE" "$target"
+}
+
+package
