d02e5c822a8207e3e9131c8506dfa32fb64faf63
Shinmera <shinmera@tymoon.eu>
2016-06-09 17:01:22 +0200

    Copy ld-linux.so and use it in linux launch scripts

--- sbcl/lin/sbcl.sh
+++ sbcl/lin/sbcl.sh
@@ -3,6 +3,4 @@ SCRIPT=$(dirname $(readlink -f "$0"))
 export ROOT=${ROOT:-$(readlink -f "$SCRIPT/../../")/}
 export SBCL_HOME=$ROOT/sbcl/lin/lib/sbcl/
 
-export LD_LIBRARY_PATH=$ROOT/usr/lib/:$LD_LIBRARY_PATH
-
-"$SCRIPT/bin/sbcl" --no-sysinit --userinit "$ROOT/config/sbcl-init.lisp" "$@"
+"$ROOT/usr/lib/ld-linux.so" --library-path "$ROOT/usr/lib/" "$SCRIPT/bin/sbcl" --no-sysinit --userinit "$ROOT/config/sbcl-init.lisp" "$@"
