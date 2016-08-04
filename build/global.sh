8a4c8ad4e4451a4439a77ef66ecb33c0412ca7d3
Shinmera <shinmera@tymoon.eu>
2016-08-04 10:37:18 +0200

    Make sure to create shared lib dir

--- build/global.sh
+++ build/global.sh
@@ -5,11 +5,8 @@
 readonly PROGRAM=global
 source common.sh
 
-function download() {
-    echo "Skipping download"
-}
-
 function install() {
+    mkdir -p "$SHARED_DIR/lib"
     case "$PLATFORM" in
         lin) cp -fuv "/lib64/ld-linux-x86-64.so.2" "$SHARED_DIR/lib/ld-linux.so" ;;
     esac
