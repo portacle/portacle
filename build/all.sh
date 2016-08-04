f0f0725a0fde34a0e9ec4e2f08921660bfb242f1
Shinmera <shinmera@tymoon.eu>
2016-08-04 11:11:25 +0200

    Better all script

--- build/all.sh
+++ build/all.sh
@@ -1,12 +1,46 @@
 #!/bin/bash
 
-./global.sh "$@" \
-    && ./sbcl.sh "$@" \
-    && ./asdf.sh "$@" \
-    && ./quicklisp.sh "$@" \
-    && ./emacs.sh "$@" \
-    && ./emacsd.sh "$@" \
-    && ./git.sh "$@" \
-    && ./aspell.sh "$@" \
-    && ./aspell-dicts.sh "$@" \
-    && ./package.sh
+TARGET=${1:-all}
+
+function reset() {
+    git clean -ffxd
+}
+
+function global() {
+    ./global.sh "$@"
+}
+
+function cl() {
+    ./sbcl.sh "$@" \
+        && ./asdf.sh "$@" \
+        && ./quicklisp.sh "$@"
+}
+
+function editor() {
+    ./emacs.sh "$@" \
+        && ./emacsd.sh "$@"
+}
+
+function utils() {
+    ./git.sh "$@" \
+        && ./aspell.sh "$@" \
+        && ./aspell-dicts.sh "$@" \
+}
+
+function package() {
+    ./package.sh "$@"
+}
+
+function all() {
+    global "$@" \
+        && cl "$@" \
+        && editor "$@" \
+        && utils "$@" \
+        && package "$@"
+}
+
+function main() {
+    $TARGET
+}
+
+main
