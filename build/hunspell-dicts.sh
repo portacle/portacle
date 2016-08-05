b6c50d7a5ee064b9149c12a4808923426a658b2c
Shinmera <shinmera@tymoon.eu>
2016-08-05 13:33:10 +0200

    Stubs for hunspell which went off unused.

---
+++ build/hunspell-dicts.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+readonly TAG=v1.4.1
+readonly REPOSITORY=https://github.com/hunspell/hunspell
+
+###
+
+readonly PROGRAM=hunspell
+source common.sh
+
+function prepare() {
+    cd "$SOURCE_DIR"
+    
+}
+
+function build() {
+    cd "$SOURCE_DIR"
+    
+}
+
+function install() {
+    cd "$SOURCE_DIR"
+    
+}
+
+main
