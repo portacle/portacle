ea64721301735cca321bb63d905e7da405231fcc
Shinmera <shinmera@tymoon.eu>
2016-08-07 15:23:36 +0200

    Start working on an LD_PRELOAD execve injector hook.

---
+++ build/global/ld-wrap.c
@@ -0,0 +1,33 @@
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+static int (*o_execve)(const char *filename, char *const argv[], char *const envp[]) = NULL;
+
+char **ld_wrap_argv(const char *filename, char *const argv[]){
+  char **argv_t = malloc(3*sizeof(char *));
+  argv_t[0] = "--library-path";
+  argv_t[1] = getenv("LW_LIBRARY_PATH");
+  for(int i=0; argv[i]; ++i){
+    argv_t[i+2] = argv[i];
+  }
+  return argv_t;
+}
+
+int execve(const char *filename, char *const argv[], char *const envp[]){
+  char **argv_t = ld_wrap_argv(filename, argv);
+  o_execve = o_execve ? o_execve : dlsym(RTLD_NEXT, "execve");
+  int status = o_execve(getenv("LW_LOADER_PATH"), argv_t, envp);
+  free(argv_t);
+  return status;
+}
+
+int __execve(const char *filename, char *const argv[], char *const envp[]){
+  char **argv_t = ld_wrap_argv(filename, argv);
+  o_execve = o_execve ? o_execve : dlsym(RTLD_NEXT, "__execve");
+  int status = o_execve(getenv("LW_LOADER_PATH"), argv_t, envp);
+  free(argv_t);
+  return status;
+}
