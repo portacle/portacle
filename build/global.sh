affb58cbc366591da505415be2a021dd1d701396
Shinmera <shinmera@tymoon.eu>
2016-08-07 15:23:36 +0200

    Start working on an LD_PRELOAD execve injector hook.

--- build/global.sh
+++ build/global.sh
@@ -8,11 +8,21 @@ readonly SSL_CA=https://curl.haxx.se/ca/cacert-2016-04-20.pem
 
 readonly PROGRAM=global
 source common.sh
+INSTALL_TARGET="$SHARED_DIR/lib"
+
+function build() {
+    mkdir -p "$SOURCE_DIR"
+    case "$PLATFORM" in
+        lin) gcc -fPIC -shared -o "$SOURCE_DIR/ld-wrap.so" "$SOURCE_DIR/ld-wrap.c" -ldl
+             cp -fuv "/usr/lib/ld-linux-x86-64.so.2" "$SOURCE_DIR/ld-linux.so"
+             ;;
+    esac
+}
 
 function install() {
-    mkdir -p "$SHARED_DIR/lib"
+    mkdir -p "$INSTALL_TARGET/"
     case "$PLATFORM" in
-        lin) cp -fuv "/lib64/ld-linux-x86-64.so.2" "$SHARED_DIR/lib/ld-linux.so" ;;
+        lin) ensure-installed "$INSTALL_TARGET/" "$SOURCE_DIR/ld-wrap.so" "$SOURCE_DIR/ld-linux.so" ;;
     esac
 
     mkdir -p "$SHARED_DIR/ssl"
