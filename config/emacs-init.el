cc3964eff4fb1b54c227a6f46e25ef01c9d68f4b
Shinmera <shinmera@tymoon.eu>
2016-10-10 13:19:24 +0200

    Added project management functions as per #8

--- config/emacs-init.el
+++ config/emacs-init.el
@@ -13,6 +13,9 @@
       (insert-file-contents file)
     (buffer-string)))
 
+(defun write-file-contents (contents file &optional append)
+  (write-region contents nil file append))
+
 ;; Set up paths
 (setq portacle-root (or (getenv "ROOT") (expand-file-name "~/")))
 (setq portacle-os (os-case (gnu/linux "lin") (darwin "mac") (windows-nt "win")))
@@ -148,17 +151,92 @@
       (insert "===> All done\n")
       (insert "\n Press q to close this buffer."))))
 
-(defun portacle-configure ()
+(defun portacle-configure (&key name email licence)
   (interactive)
-  (let ((name (read-string "Your name: " user-full-name))
-        (email (read-string "Your e-mail address: " user-mail-address)))
+  (let ((name (or name (read-string "Your name: " user-full-name)))
+        (email (or email (read-string "Your e-mail address: " user-mail-address)))
+        (licence (or licence (read-string "Default project licence: " (or project-default-licence "3-clause BSD")))))
     (call-process magit-git-executable nil nil t "config" "--file" (portacle-path "config/git/config") "user.name" name)
     (call-process magit-git-executable nil nil t "config" "--file" (portacle-path "config/git/config") "user.email" email)
-    (write-region (prin1-to-string `(setq user-full-name ,name)) nil (portacle-path "config/user.el") 'append)
-    (write-region (prin1-to-string `(setq user-mail-address ,email)) nil (portacle-path "config/user.el") 'append)
+    (write-file-contents (prin1-to-string `(setq user-full-name ,name)) (portacle-path "config/user.el") t)
+    (write-file-contents (prin1-to-string `(setq user-mail-address ,email)) (portacle-path "config/user.el") t)
+    (write-file-contents (prin1-to-string `(setq project-default-licence ,email)) (portacle-path "config/user.el") t)
     (setq user-full-name name)
     (setq user-mail-address email)
     (message "User information set.")))
+
+(defun replace-project-variables (string &optional vars)
+  (let ((vars '(user-full-name user-mail-address
+                project-name project-description
+                project-licence year month day))
+        (year (format-time-string "%Y"))
+        (month (format-time-string "%m"))
+        (day (format-time-string "%d"))
+        ;; Case-sensitive regexp mathcing
+        (case-fold-search nil))
+    (dolist (var vars string)
+      (setq string (replace-regexp-in-string (upcase (symbol-name var)) (symbol-value var) string t t)))))
+
+(defun maybe-update-quicklisp-db ()
+  (interactive)
+  (cond ((slime-connected-p)
+         (message "Updating Quicklisp DB...")
+         (slime-eval '(ql:register-local-projects))
+         (message "Quicklisp DB updated."))
+        (t
+         (message "Slime not connected, cannot update."))))
+
+(defun create-project (&key name description licence)
+  (interactive)
+  (let* ((project-name (or name (read-string "Project name: ")))
+         (project-description (or description (read-string "Project description: ")))
+         (project-licence (or licence (read-string "Project licence: " project-default-licence)))
+         (dir (portacle-path (concat "projects/" project-name)))
+         (skeleton (portacle-path "config/skeleton/")))
+    (cond ((file-exists-p dir)
+           (message "A project with that name already exists."))
+          (t
+           (message "Creating project skeleton...")
+           (labels ((rcopy (from to)
+                      (make-directory to t)
+                      (dolist (template (directory-files from))
+                        (let ((file (concat from "/" template))
+                              (destfile (concat to "/" (replace-project-variables template))))
+                          (cond ((file-directory-p file)
+                                 (rcopy file destfile))
+                                (t
+                                 (write-file-contents (replace-project-variables (file-contents file))
+                                                      destfile)))))))
+             (rcopy skeleton dir))
+           (magit-init dir)
+           (maybe-update-quicklisp-db)
+           (message "Project created.")))))
+
+(defun clone-project (url &optional name)
+  (interactive)
+  (let ((url (or url (read-string "Project URL: ")))
+        (path (url-filename (url-generic-parse-url url)))
+        (name (or name (car (last (split-string path "/")))))
+        (dir (portacle-path (concat "projects/" name))))
+    (cond ((file-exists-p dir)
+           (message "A project with that name already exists."))
+          (t
+           (message "Cloning project...")
+           (magit-clone url dir)
+           (maybe-update-quicklisp-db)
+           (message "Project cloned.")))))
+
+(defun remove-project (&optional name)
+  (interactive)
+  (let* ((name (or name (read-string "Project name: ")))
+         (dir (portacle-path (concat "projects/" name))))
+    (cond ((file-exists-p dir)
+           (message "Deleting project directory...")
+           (delete-directory dir T)
+           (maybe-update-quicklisp-db)
+           (message "Project removed."))
+          (t
+           (message "No such project found.")))))
 
 ;; Other fixes
 (when (eql system-type 'windows-nt)
