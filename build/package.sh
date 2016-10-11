715f643c40f5a037a81ccbc8612f21d8bd4cfe06
Shinmera <shinmera@tymoon.eu>
2016-10-11 19:42:01 +0200

    Only clear out subfolders in projects

--- build/package.sh
+++ build/package.sh
@@ -36,7 +36,7 @@ function build() {
     fi
 
     ## Clean out known artefacts
-    rm -rf "$INSTALL_TARGET/asdf/cache" "$INSTALL_TARGET/projects/"* "$INSTALL_TARGET/package"
+    rm -rf "$INSTALL_TARGET/asdf/cache" "$INSTALL_TARGET/projects/"*/ "$INSTALL_TARGET/package"
 }
 
 function install() {
