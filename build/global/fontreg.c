2adbaacbe192fff7cbefe5d3d44dd79692aa8560
Shinmera <shinmera@tymoon.eu>
2016-10-14 13:53:46 +0200

    Fix fontreg to work

--- build/global/fontreg.c
+++ build/global/fontreg.c
@@ -1,17 +1,30 @@
 #include<windows.h>
 
-int add_font(char *font){
-  if(AddFontResource(font) == 0){
-    return 0;
-  }else{
-    SendMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
+int resolve_path(char *path, char *resolved){
+  int len = GetFullPathName(path, MAX_PATH, resolved, 0);
+  if(MAX_PATH < len)
     return 1;
-  }
+  if(len <= 0)
+    return 2;
+  return 0;
+}
+
+int add_font(char *file){
+  char font[MAX_PATH] = "";
+
+  if(resolve_path(file, font) != 0)
+    return 1;
+    
+  if(AddFontResource(font) == 0)
+    return 2;
+
+  SendMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
+  return 0;
 }
 
 int main(int argc, char **argv){
-  for(int i=0; i<argc; ++i){
-    if(add_font(argv[i]) == 0)
+  for(int i=1; i<argc; ++i){
+    if(add_font(argv[i]) != 0)
       return i+1;
   }
   return 0;
