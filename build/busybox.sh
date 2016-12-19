07bb670811afe603aaf8a2b97ec0dbe15ac63d4e
Shinmera <shinmera@tymoon.eu>
2016-12-20 00:38:59 +0100

    Busybox build. (not integrated completely yet..)

---
+++ build/busybox.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+readonly TAG=1_25_1
+readonly REPOSITORY=git://busybox.net/busybox.git
+
+###
+
+readonly PROGRAM=busybox
+source common.sh
+INSTALL_DIR="$SHARED_DIR/$PLATFORM"
+
+function prepare() {
+    cd "$SOURCE_DIR"
+    make defconfig
+    echo -e "CONFIG_INSTALL_NO_USR=y\n$(cat .config)" > .config
+    echo -e "CONFIG_STATIC=y\n$(cat .config)" > .config
+}
+
+function build() {
+    cd "$SOURCE_DIR"
+    make -j $MAXCPUS \
+         || eexit "The build failed. Please check the output for error messages."
+}
+
+function install() {
+    cd "$SOURCE_DIR"
+    make CONFIG_PREFIX="$INSTALL_DIR" install\
+        || eexit "The install failed. Please check the output for error messages."
+}
+
+main
