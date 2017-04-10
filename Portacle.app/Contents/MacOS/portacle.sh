fde43281a7d91661dccede54bf6e49cc5f055b6d
Shinmera <shinmera@tymoon.eu>
2017-04-10 19:04:50 +0200

    Fix OS X launcher shim for Python3 (#22)

--- Portacle.app/Contents/MacOS/portacle.sh
+++ Portacle.app/Contents/MacOS/portacle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function mreadlink() {
-    python -c 'import os, sys; print os.path.realpath(sys.argv[1])' $1
+    python -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "$1"
 }
 
 SCRIPT=$(dirname $(mreadlink "$0"))
