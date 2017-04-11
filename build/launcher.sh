d20bbaf80415a9c92822720eecd116899c51d194
Shinmera <shinmera@tymoon.eu>
2017-04-11 11:38:40 +0200

    Remove fontreg copy from launcher and add hunspell alias.

--- build/launcher.sh
+++ build/launcher.sh
@@ -17,9 +17,9 @@ function install() {
     cd "$SOURCE_DIR/build"
     mkdir -p "$INSTALL_DIR"
     case "$PLATFORM" in
-        win) cp "portacle.exe" "fontreg.exe" "$INSTALL_DIR/" ;;
-        lin) cp "portacle" "fontreg" "ld-wrap.so" "$INSTALL_DIR/" ;;
-        mac) cp "portacle" "fontreg" "$INSTALL_DIR/" ;;
+        win) cp "portacle.exe" "$INSTALL_DIR/" ;;
+        lin) cp "portacle" "ld-wrap.so" "$INSTALL_DIR/" ;;
+        mac) cp "portacle" "$INSTALL_DIR/" ;;
     esac
 
     local postfix=""
@@ -29,6 +29,7 @@ function install() {
     ln -f "$INSTALL_DIR/portacle${postfix}" "$SHARED_BIN_DIR/sbcl${postfix}"
     ln -f "$INSTALL_DIR/portacle${postfix}" "$SHARED_BIN_DIR/git${postfix}"
     ln -f "$INSTALL_DIR/portacle${postfix}" "$SHARED_BIN_DIR/emacs${postfix}"
+    ln -f "$INSTALL_DIR/portacle${postfix}" "$SHARED_BIN_DIR/hunspell${postfix}"
     ln -f "$INSTALL_DIR/portacle${postfix}" "$SHARED_BIN_DIR/portacle${postfix}"
 }
 
