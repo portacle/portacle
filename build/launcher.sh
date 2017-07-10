63ae217dc5f51a4a9a9c2acb6cf83205bbc739dc
Shinmera <shinmera@tymoon.eu>
2017-07-10 16:26:13 +0200

    Set credentials as git credential helper.

--- build/launcher.sh
+++ build/launcher.sh
@@ -19,15 +19,20 @@ function install() {
     mkdir -p "$SHARED_BIN_DIR"
     
     case "$PLATFORM" in
-        win) cp "portacle.exe" "credentials.exe" "$INSTALL_DIR/" ;;
-        lin) cp "portacle" "credentials" "ld-wrap.so" "$INSTALL_DIR/" ;;
-        mac) cp "portacle" "credentials" "$INSTALL_DIR/" ;;
+        win) cp "portacle.exe" "credentials" "$INSTALL_DIR/"
+             ensure-dependencies "credentials" ;;
+        lin) cp "portacle" "credentials" "ld-wrap.so" "$INSTALL_DIR/"
+             ensure-dependencies "credentials" ;;
+        mac) cp "portacle" "credentials" "$INSTALL_DIR/"
+             ensure-dependencies "credentials" ;;
     esac
 
     local postfix=""
     case "$PLATFORM" in
         win) postfix=".exe" ;;
     esac
+
+    ln -f "$INSTALL_DIR/credentials${postfix}" "$SHARED_BIN_DIR/git-credential-portacle${postfix}"
     ln -f "$INSTALL_DIR/portacle${postfix}" "$SHARED_BIN_DIR/sbcl${postfix}"
     ln -f "$INSTALL_DIR/portacle${postfix}" "$SHARED_BIN_DIR/git${postfix}"
     ln -f "$INSTALL_DIR/portacle${postfix}" "$SHARED_BIN_DIR/emacs${postfix}"
