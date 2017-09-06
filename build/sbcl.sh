a08ee0de286e6467593114798e4b83217d063197
Shinmera <shinmera@tymoon.eu>
2017-09-06 11:21:10 +0200

    Downgrade SBCL to 1.3.17 to fix on OS X 10.11

--- build/sbcl.sh
+++ build/sbcl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readonly TAG=sbcl-1.3.21
+readonly TAG=sbcl-1.3.17
 readonly REPOSITORY=https://github.com/sbcl/sbcl.git
 readonly DISABLED_FEATURES=":largefile :sb-ldb"
 readonly ENABLED_FEATURES=":sb-thread :sb-core-compression"
