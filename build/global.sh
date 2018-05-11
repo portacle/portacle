51c2034f912540bce826c1cd6fcbdf93c0a438b5
Shinmera <shinmera@tymoon.eu>
2018-05-11 09:42:54 +0200

    Ship locale (fixes #63, along with portacle/launcher@367e22f)

--- build/global.sh
+++ build/global.sh
@@ -30,21 +30,28 @@ function prepare() {
 function install() {
     mkdir -p "$SHARED_LIB_DIR/"
     mkdir -p "$SHARED_BIN_DIR/"
+    mkdir -p "$PORTACLE_DIR/all/ssl"
+    mkdir -p "$PORTACLE_DIR/all/locale/"
     
     case "$PLATFORM" in
         lin) ensure-shared-libraries "$SHARED_LIB_DIR/" "libnss_"
              ensure-dependencies $SHARED_LIB_DIR/libnss_*
+             localedef -f UTF-8 -i en_US "$PORTACLE_DIR/all/locale/en_US.UTF-8" \
+                 || eexit "Failed to generate locale"
              case "$ARCH" in
                  64) cp -fv /lib64/ld-linux-*.so.* "$SHARED_LIB_DIR/ld-linux.so" \
                            || eexit "Failed to copy ld-linux.so" ;;
                  32) cp -fv /lib/ld-linux-*.so.* "$SHARED_LIB_DIR/ld-linux.so" \
                            || eexit "Failed to copy ld-linux.so" ;;
              esac ;;
+        mac) ucp "/usr/share/locale/en_US.UTF-8" "$PORTACLE_DIR/all/locale/" \
+                   || eexit "Failed to copy locale"
+             ;;
         win) cp -fv "/usr/bin/gzip" "$SHARED_BIN_DIR/" \
-                   || eexit "Failed to copy gzip" ;;
+                   || eexit "Failed to copy gzip"
+             ;;
     esac
 
-    mkdir -p "$PORTACLE_DIR/all/ssl"
     ensure-installed "$PORTACLE_DIR/all/ssl/" "$SOURCE_DIR/ca-bundle.crt"
     ensure-installed "$PORTACLE_DIR/all/fonts/" "$SOURCE_DIR/fonts"/*
 }
